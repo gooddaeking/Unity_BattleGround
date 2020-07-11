f39952168d0414511d228d4b7551c9c2f12e16cc
gooddaeking <gooddaeking@gmail.com>
2020-07-11 22:24:22 +0900

    PluggableAI 스크립트 완료 Enemey States 설정완료

---
+++ battleground/Assets/1.Scripts/Enemy/StateMachine/Action/SpotFocusAction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="PluggableAI/Actions/Spot Focus")]
+public class SpotFocusAction : Action
+{
+    public override void Act(StateController controller)
+    {
+        controller.nav.destination = controller.personalTarget;
+        controller.nav.speed = 0;
+    }
+}
